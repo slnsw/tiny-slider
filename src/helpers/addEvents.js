9ff398b224c6bc121d315dc04fea583f90279909
Priscila Martinez <priscila.mtz.fraire@gmail.com>
2021-05-03 19:05:22 -0500

    fix: use passive scroll event listeners if `preventScrolling` is `auto`

--- src/helpers/addEvents.js
+++ src/helpers/addEvents.js
@@ -2,7 +2,7 @@ import { passiveOption } from './passiveOption.js';
 
 export function addEvents(el, obj, preventScrolling) {
   for (var prop in obj) {
-    var option = ['touchstart', 'touchmove'].indexOf(prop) >= 0 && !preventScrolling ? passiveOption : false;
+    var option = ['touchstart', 'touchmove'].indexOf(prop) >= 0 && (!preventScrolling || preventScrolling === 'auto') ? passiveOption : false;
     el.addEventListener(prop, obj[prop], option);
   }
 }
